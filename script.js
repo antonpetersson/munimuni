f96539aecc0231d4417d897c365d8b84e3463527
antonpetersson <32762201+antonpetersson@users.noreply.github.com>
2017-12-27 17:59:59 +0100

    Fixat så produktet filterars på huvudkategorier.

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ fetch("./huvudkategorier.json")
     .then(function(huvudkategorier) {
         mainCatList = huvudkategorier;
         printMainCat();
-
     });
+
 fetch("./underkategorier.json")
     .then(function(response) {
         return response.json();
@@ -20,13 +20,18 @@ fetch("./underkategorier.json")
 
     });
 
+fetch("./produkter.json")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(produkter) {
+        productList = produkter;
+        
+
+    });
 
 
 
-$(".mainMenuItemClass").click(function(){ 
-        console.log("hejdå")
-       
-    }); 
 
 
 
@@ -38,22 +43,37 @@ function printMainCat(){
     $(".mainMenuList").append("<li><a href='#'>Start</a></li>");
     for(var i = 0; i < mainCatList.length; i++){
         
-        var mainCatName = "<li class='mainMenuItemClass' onclick='onClickMain(" + [i] + ")'><a href='#'>" + mainCatList[i].mainCategory + "</a></li>";
+        var mainCatName = "<li class='mainMenuItemClass' onclick='printSubCat(" + [i] + "); printProductList(" + [i] + ")'><a href='#'>" + mainCatList[i].mainCategory + "</a></li>";
         $(".mainMenuList").append(mainCatName);
     }
         $(".mainMenuList").append("<li><a href='#'>Kontakt</a></li>");
         $(".mainMenuList").append("<li><a href='#'>Information</a></li>");
         $(".mainMenuList").append("<li><a href='#'>Kundvagn</a></li>");      
 
-        onClickMain = function(i){      
+        printSubCat = function(i){      
             $(".subMenuList").html("");    
             for(var index = 0; index < subCatList.length; index++){
-            
+                var subCatName = "<li><a href='#'>" + subCatList[index].subCategory + "</a></li>";
+
                 if (subCatList[index].mainCategory == i+1){
-                    $(".subMenuList").append("<li><a href='#'>" + subCatList[index].subCategory + "</a></li>")
+                    $(".subMenuList").append(subCatName);
                 }
            }
-           
+        }
+        printProductList = function(i){
+            $(".main").html("");
+            for(var index = 0; index < productList.length; index++){
+                
+                var productCardName = "<h2>" + productList[index].prodName  + "</h2>";
+                var productCardPrice = "<p>" + productList[index].prodPrice + " kr</p>"
+
+                var productCard = "<div class='productCard'>" + productCardName + productCardPrice + "</div>";
+
+                if (productList[index].mainCategory == i+1){
+                    $(".main").append(productCard);
+                }
+               
+            }
         }
 
 
