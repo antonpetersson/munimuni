3ae08bfd18a0896b414d5b319b61be36e1881ae9
antonpetersson <antonpetersson@users.noreply.github.com>
2018-01-04 19:14:24 +0100

    Stylat kundvagnsikonen och fixat en räknare till den

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ var mainCatList;
 var shoppingCart = [];
 
 
-
-
-
 fetch("./huvudkategorier.json")
     .then(function(response) {
         return response.json();
@@ -21,8 +18,6 @@ fetch("./underkategorier.json")
     })
     .then(function(underkategorier) {
         subCatList = underkategorier;
-        
-
     });
 
 fetch("./produkter.json")
@@ -31,17 +26,7 @@ fetch("./produkter.json")
     })
     .then(function(produkter) {
         productList = produkter;
-        
-
     });
-
-
-
-
-
-
-
-
 
 
 function printMainCat(){
@@ -51,112 +36,128 @@ function printMainCat(){
         var mainCatName = "<li class='mainMenuItemClass' onclick='printSubCat(" + [i] + "); printProductList(" + [i] + ")'><a href='#'>" + mainCatList[i].mainCategory + "</a></li>";
         $(".mainMenuList").append(mainCatName);
     }
-        $(".mainMenuList").append("<li><a href='#'>Kontakt</a></li>");
-        $(".mainMenuList").append("<li><a href='#'>Information</a></li>");
-        $(".mainMenuList").append("<li class='cartbutton'><a href='#'>Kundvagn</a></li>");      
-        $(".cartbutton").click(function(){
-            showCart();
-        });
+    $(".mainMenuList").append("<li><a href='#'>Kontakt</a></li>");
+    $(".mainMenuList").append("<li><a href='#'>Information</a></li>");
+    //$(".mainMenuList").append("<li class='cartLink'><a href='#'><div class='cartCounter'></div></a></li>");  
+    
+    $(".cartLink").hover(function(){
+            $(".cartLink").css("background-image", "url(img/cartHover.png)");
+            $(this).css('cursor','pointer');
+    }, function() {
+        if (
+            shoppingCart.length != 0){$(".cartLink").css("background-image", "url(img/cartFull.png)");
+        }
+        else{
+            $(".cartLink").css("background-image", "url(img/cart.png)");
+        }  
+    });
+    $(".cartLink").click(function(){
+        showCart();
+    });
 
-        printSubCat = function(i){      
-            $(".subMenuList").html("");    
-            for(var index = 0; index < subCatList.length; index++){
-                var subCatName = "<li onclick='printProductList(" + subCatList[index].id + ")'><a href='#'>" + subCatList[index].subCategory + "</a></li>";
+    printSubCat = function(i){      
+        $(".subMenuList").html("");    
+        for(var index = 0; index < subCatList.length; index++){
+            var subCatName = "<li onclick='printProductList(" + subCatList[index].id + ")'><a href='#'>" + subCatList[index].subCategory + "</a></li>";
 
-                if (subCatList[index].mainCategory == i+1){
-                    $(".subMenuList").append(subCatName);
-                }
+            if (subCatList[index].mainCategory == i+1){
+                $(".subMenuList").append(subCatName);
             }
         }
-        printProductList = function(i){
-            $(".main").html("");
+    }
+    printProductList = function(i){
+        $(".main").html("");
+        
+        for(var index = 0; index < productList.length; index++){
             
-            for(var index = 0; index < productList.length; index++){
-                
-                var productCardName = "<h2>" + productList[index].prodName  + "</h2>";
-                var productCardPrice = "<p>" + productList[index].prodPrice + " kr</p>";
-                var productCardImage = "<img class='productCardImg' onclick='showProduct(" + productList[index].id + ")' src='img/products/" + productList[index].prodImg + "'>";
+            var productCardName = "<h2>" + productList[index].prodName  + "</h2>";
+            var productCardPrice = "<p>" + productList[index].prodPrice + " kr</p>";
+            var productCardImage = "<img class='productCardImg' onclick='showProduct(" + productList[index].id + ")' src='img/products/" + productList[index].prodImg + "'>";
 
-                var productCard = "<div class='productCard'>" + productCardImage + productCardName + productCardPrice + "</div>";
+            var productCard = "<div class='productCard'>" + productCardImage + productCardName + productCardPrice + "</div>";
 
-                if (productList[index].mainCategory == i+1){
-                    $(".main").append(productCard);
-                }
-                else if (productList[index].subCategory == i ){
-                    $(".main").append(productCard);
-                }
+            if (productList[index].mainCategory == i+1){
+                $(".main").append(productCard);
+            }
+            else if (productList[index].subCategory == i ){
+                $(".main").append(productCard);
             }
         }
-        showProduct = function(i){
-            $(".main").html("");
-            
-            for(var index = 0; index < productList.length; index++){
-                var productName = "<h2>" + productList[index].prodName  + "</h2>";
-                var productPrice = "<p>" + productList[index].prodPrice + " kr</p>";
-                var productImage = "<img class='productImg' alt='" + productList[index].prodName + "' src='img/products/" + productList[index].prodImg + "'>";
-                var productDescription = "<p>" + productList[index].prodDesc + "</p>";
-                var cartButton = "<button class='cartButton' onclick='addToCart(" + productList[index].id + ")'>Lägg i kundvagn</button>";
+    }
+    showProduct = function(i){
+        $(".main").html("");
+        
+        for(var index = 0; index < productList.length; index++){
+            var productName = "<h2>" + productList[index].prodName  + "</h2>";
+            var productPrice = "<p>" + productList[index].prodPrice + " kr</p>";
+            var productImage = "<img class='productImg' alt='" + productList[index].prodName + "' src='img/products/" + productList[index].prodImg + "'>";
+            var productDescription = "<p>" + productList[index].prodDesc + "</p>";
+            var cartButton = "<button class='cartButton' onclick='addToCart(" + productList[index].id + ")'>Lägg i kundvagn</button>";
 
-                var productContainer = "<div class='productContainer'>" + productName + "<hr class='productHR'>" + productPrice + cartButton + productDescription + "</div>";
+            var productContainer = "<div class='productContainer'>" + productName + "<hr class='productHR'>" + productPrice + cartButton + productDescription + "</div>";
 
-                if (productList[index].id == i){
-                    $(".main").append(productImage);
-                    $(".main").append(productContainer);
-                }
+            if (productList[index].id == i){
+                $(".main").append(productImage);
+                $(".main").append(productContainer);
             }
         }
+    }
+    addToCart = function(i){
+        
+        var productName = productList[i-1].prodName
+        var productPrice = productList[i-1].prodPrice
+        
 
-        addToCart = function(i){
-            
-            var productName = productList[i-1].prodName
-            var productPrice = productList[i-1].prodPrice
-            
+        shoppingCart.push(productList[i-1]);
+        $(".cartCounter").html(shoppingCart.length);
+        $(".cartLink").css("background-image", "url(img/cartFull.png)");
+    }
 
-            shoppingCart.push(productList[i-1]);
-            console.log(shoppingCart);
+    showCart = function(){
+        $(".main").html("<h2>Kundvagn</h2><hr class='productHR'>");
+
+        var json_str = JSON.stringify(shoppingCart);
+        localStorage.shoppingCart = json_str; 
+        
+        var loopCart = JSON.parse(localStorage.shoppingCart);
+
+        
+        var cartListProdName = "<ul class='cartListProdName'>";
+        var cartListProdPrice = "<ul>";
+        var cartListRemove = "<ul class='cartListRemove'>";
+
+        for(var i = 0; i < loopCart.length; i++){
+            cartListProdName += "<li>" + loopCart[i].prodName + "</li>";
+            cartListProdPrice += "<li>" + loopCart[i].prodPrice + " kr</li>";
+            cartListRemove += "<li><a href='#' onClick='delCartItem(" + i + ")'>Ta bort</a></li>";
         }
 
-        showCart = function(){
-            $(".main").html("<h2>Kundvagn</h2><hr class='productHR'>");
-
-            var json_str = JSON.stringify(shoppingCart);
-            localStorage.shoppingCart = json_str; 
-            
-            var loopCart = JSON.parse(localStorage.shoppingCart);
-
-            
-            var cartListProdName = "<ul class='cartListProdName'>";
-            var cartListProdPrice = "<ul>";
-            var cartListRemove = "<ul class='cartListRemove'>";
-
-            for(var i = 0; i < loopCart.length; i++){
-                cartListProdName += "<li>" + loopCart[i].prodName + "</li>";
-                cartListProdPrice += "<li>" + loopCart[i].prodPrice + " kr</li>";
-                cartListRemove += "<li><a href='#' onClick='delCartItem(" + i + ")'>Ta bort</a></li>";
-            }
-
-            var priceTotal = 55;
-            for(var i = 0; i < shoppingCart.length; i++) {
-                priceTotal += shoppingCart[i].prodPrice;
-            }
-
-            cartListProdName += "<li>Frakt</li></ul>"
-            cartListProdPrice += "<li>55 kr</li></ul>"
-            cartListRemove += "</ul>"
-            
-       
-            $(".main").append("<div class='cartList'></div><div class='cartSummary'></div>");
-            $(".cartList").append(cartListProdName + cartListProdPrice + cartListRemove);
-
-            var checkOutButton = "<button class='cartButton' onclick='checkOut()'>Gå till kassan</button>";
-            $(".cartSummary").append("<h3>Totalpris: " + priceTotal + " kr </br>" + checkOutButton);
-
+        var priceTotal = 55;
+        for(var i = 0; i < shoppingCart.length; i++) {
+            priceTotal += shoppingCart[i].prodPrice;
         }
 
-        delCartItem = function(i){
-            shoppingCart.splice(i, 1);
-            showCart();
+        cartListProdName += "<li>Frakt</li></ul>"
+        cartListProdPrice += "<li>55 kr</li></ul>"
+        cartListRemove += "</ul>"
+        
+    
+        $(".main").append("<div class='cartList'></div><div class='cartSummary'></div>");
+        $(".cartList").append(cartListProdName + cartListProdPrice + cartListRemove);
+
+        var checkOutButton = "<button class='cartButton' onclick='checkOut()'>Gå till kassan</button>";
+        $(".cartSummary").append("<h3>Totalpris: " + priceTotal + " kr </br>" + checkOutButton);
+
+    }
+
+    delCartItem = function(i){
+        shoppingCart.splice(i, 1);
+        showCart();
+        $(".cartCounter").html(shoppingCart.length);
+        if (shoppingCart.length <= 0){
+            $(".cartLink").css("background-image", "url(img/cart.png)");
         }
+    }
 
         
 
